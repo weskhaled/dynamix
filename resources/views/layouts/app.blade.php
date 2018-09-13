cb50d24b1a8083aab076a5613f4833a677ba3cc7
Khaled Weslati <weskhaled@gmail.com>
2018-09-13 18:29:23 +0100

    from mac

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,22 +2,24 @@
 <html {!! get_language_attributes() !!}>
   @include('partials.head')
   <body @php body_class() @endphp>
-    @php do_action('get_header') @endphp
-    @include('partials.header')
-    <div class="wrap container" role="document">
-      <div class="content">
-        <main class="main">
-          @yield('content')
-        </main>
-        @if (App\display_sidebar())
-          <aside class="sidebar">
-            @include('partials.sidebar')
-          </aside>
-        @endif
+    <div class="page-content">
+      @php do_action('get_header') @endphp
+      @include('partials.header')
+      <div class="wrap container" role="document">
+        <div class="content">
+          <main class="main">
+            @yield('content')
+          </main>
+          @if (App\display_sidebar())
+            <aside class="sidebar">
+              @include('partials.sidebar')
+            </aside>
+          @endif
+        </div>
       </div>
+      @php do_action('get_footer') @endphp
+      @include('partials.footer')
+      @php wp_footer() @endphp 
     </div>
-    @php do_action('get_footer') @endphp
-    @include('partials.footer')
-    @php wp_footer() @endphp
   </body>
 </html>
