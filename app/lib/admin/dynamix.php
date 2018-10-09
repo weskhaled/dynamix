d61fecda9500da926d45f94609eecb76f7e574fc
DESKTOP-4E7GB0R\Peaksource <khaled@peaksource.tn>
2018-10-09 18:01:27 +0100

    add element-ui to admin

--- app/lib/admin/dynamix.php
+++ app/lib/admin/dynamix.php
@@ -1,4 +1,5 @@
 <?php
+namespace App;
 /**
  * Add Menu Admin
  */
@@ -21,4 +22,22 @@ add_action('admin_menu', function () {
         require_once 'pages/index.php';
         // @include('pages.index');
     });
+});
+// function my_enqueue($hook) {
+//     // Only add to the edit.php admin page.
+//     // See WP docs.
+//     if ('edit.php' !== $hook) {
+//         return;
+//     }
+//     wp_enqueue_script('my_custom_script', plugin_dir_url(__FILE__) . '/myscript.js');
+// }
+
+// add_action('admin_enqueue_scripts', 'my_enqueue');
+
+/**
+ * Customizer JS
+ */
+add_action('admin_enqueue_scripts', function () {
+	wp_enqueue_style('sage/admin/main.css', asset_path('styles/admin.css'), false, null);
+    wp_enqueue_script('sage/admin/main.js', asset_path('scripts/admin.js'), [], null, true);
 });
--- app/lib/admin/pages/index.php
+++ app/lib/admin/pages/index.php
@@ -1,1 +1,44 @@
-<h1>Admin Pages</h1>
+<div id="appadmin">
+    <el-container>
+        <el-header>
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown button
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </div>
+            <el-dropdown>
+                <i class="el-icon-setting" style="margin-right: 15px"></i>
+                <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item>View</el-dropdown-item>
+                <el-dropdown-item>Add</el-dropdown-item>
+                <el-dropdown-item>Delete</el-dropdown-item>
+                </el-dropdown-menu>
+            </el-dropdown>
+            <span>Tom</span>
+        </el-header>
+        <el-main>
+        <h1 class="test">Admin Pages</h1>
+            <Card></Card>
+            <div class="block">
+                <span class="demonstration">Start date time 12:00:00, end date time 08:00:00</span>
+                <el-date-picker v-model="value7" size="small"
+                                type="datetimerange"
+                                align="right"
+                                start-placeholder="Start Date"
+                                end-placeholder="End Date"
+                                :default-time="['12:00:00', '08:00:00']">
+                </el-date-picker>
+            </div>
+            <el-button @click="visible = true" size="small">Button</el-button>
+        </el-main>
+    </el-container>
+    <!-- dialog -->
+    <el-dialog :visible.sync="visible" title="Hello world">
+        <p>Try Element</p>
+    </el-dialog>
+</div>
