7383b6662ecbbcb3d5573a541b76a32b8a4f9611
Khaled Weslati <weskhaled@gmail.com>
2018-10-23 23:16:00 +0100

    get medias to admin

--- resources/assets/scripts/admin/vue/components/about.vue
+++ resources/assets/scripts/admin/vue/components/about.vue
@@ -11,6 +11,7 @@
                       :action="wpApiSettings.root+'dynamix/v1/logo'"
                       :on-preview="handlePreview"
                       :on-remove="handleRemove"
+                      :on-success="handleSuccess"
                       :file-list="fileList"
                       :multiple="false"
                       :headers="{ 'X-WP-Nonce': wpApiSettings.nonce }"
@@ -24,6 +25,19 @@
             <el-tab-pane label="Config" name="second">Config</el-tab-pane>
             <el-tab-pane label="Role" name="third">Role</el-tab-pane>
             <el-tab-pane label="Task" name="fourth">Task</el-tab-pane>
+            <el-tab-pane label="Medias" name="5">
+
+              <el-row :gutter="15" class="media-imgs">
+                <el-col :span="6" class="mb-3" v-for="(media, index) in allmedias" :key="index">
+                    <el-card shadow="hover">
+                      <div class="madia-thumb">
+                            <img :src="media.url" class="img-fluid" :alt="media.name">                      
+                      </div>
+                    </el-card>
+                </el-col>
+              </el-row>
+
+            </el-tab-pane>
           </el-tabs>
       </el-col>
     </el-row>
@@ -38,10 +52,14 @@ import axios from 'axios';
            activeName: 'first',
            fileList: [],
            wpApiSettings: wpApiSettings,
+           allmedias : [],
       }),
       methods: {
         getslogo(){
           return axios.get(wpApiSettings.root+'dynamix/v1/mods',{},{headers: { 'X-WP-Nonce': wpApiSettings.nonce }})  
+        },
+        getallmedias(){
+          return axios.post(wpApiSettings.root+'dynamix/v1/allmedias',{},{headers: { 'X-WP-Nonce': wpApiSettings.nonce }})  
         },
         handleClick(tab) {
           console.log(tab);
@@ -52,11 +70,29 @@ import axios from 'axios';
         handlePreview(file) {
           console.log(file);
         },
+        handleSuccess(response) {
+          let self = this;
+          console.log(response);
+          let imgs = response.image;
+          imgs.forEach(function (value, i) {
+            console.log('%d: %s', i, value);
+            self.fileList.push({'name': i, 'url':value});
+          });
+        },
+        handlePictureCardPreview(file) {
+          console.log(file);
+          // this.dialogImageUrl = file.url;
+          // this.dialogVisible = true;
+        },
       },
       mounted: function() {
         this.getslogo().then((response) => {
             console.log(response.data);
             this.fileList.push({'name': 'logo','url':response.data.upload_logo});
+        }),
+        this.getallmedias().then((response) => {
+            console.log(response.data);
+            this.allmedias = response.data;
         })  
       },
     }
